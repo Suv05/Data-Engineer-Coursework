daa4cb9eb75c2dc88dd7bbd1d0b6c207c3fc26e2
suvam <suvampravu@gmail.com>
2024-09-22 03:24:02 +0530

    Day1 in python

---
+++ Day-1.py
@@ -0,0 +1,14 @@
+l1 = [8, 2.3, ["hello", "ram"]]
+print(type(l1))
+
+d1 = {"name": "sakhi",
+      "age": 20,
+      "female": True
+      }
+
+print(type(d1))
+
+t1 = (("parrot", "line"), ("vbm", 4))
+print(type(t1))
+
+#Day-6 in the play list finished
