0f2eb01911cb0365a91e34b14ffa7d70c13c3978
suvam <suvampravu@gmail.com>
2024-09-28 02:46:09 +0530

    commit chnages

---
+++ Day-2.py
@@ -0,0 +1,24 @@
+# a = int(input("Enter 1st number"))
+# b = int(input("Enter 2nd number"))
+#
+# print("power", a ** b)
+# print("round like output", a // b)
+# print("mod", a % b)
+
+# implicit type casting
+c = 1.5
+d = 8
+print(c + d)
+
+name = '''
+hello 
+good 
+'''
+
+a = "suvam"
+print(a[0])
+
+for char in a:
+    print(char)
+
+# finished till day-11 in playlist
