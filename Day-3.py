6f436c0073963f35bed88e2316c6adf8d68619b8
suvam <suvampravu@gmail.com>
2024-10-01 22:14:01 +0530

    Day2 in python

---
+++ Day-3.py
@@ -0,0 +1,7 @@
+# strings slicing
+name = "harry"
+
+print(name[0:4])  # 4 will not include
+print(name[:5])
+print(name[:len(name) - 1])  # -1 means len(name)-1
+print(name[-1:-4]) #not printing anything why 🧐
