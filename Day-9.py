1cc0f140a76150baf349c7216ba17b5e49b09296
suvam <suvampravu@gmail.com>
2024-10-02 22:04:24 +0530

    commit

---
+++ Day-9.py
@@ -0,0 +1,14 @@
+# break and continue video-19
+
+# for i in range(12):
+#     print(i, "ok")
+#     if i == 10:
+#         break
+# print("broken")
+
+for i in range(12):
+    if i == 10:
+        print("ankita on ok")
+        continue
+    print(i, "ok")
+print("broken")
