6a1c35faa8ca580ad744d04d78302c6f03a5c7ae
suvam <suvampravu@gmail.com>
2024-10-02 22:04:19 +0530

    commit

---
+++ Day-8.py
@@ -0,0 +1,16 @@
+# Python while loop
+count = 5
+while count > 0:
+    print("I am hungry",count,"th")
+    count = count - 1
+
+#while with else
+# count = 15
+# while count > 0:
+#     print("I am hungry",count,"th")
+#     count = count - 1
+# else:
+#     print("I am inside else")
+
+
+#video 18 finished
