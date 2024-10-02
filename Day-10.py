7a1b9b43af6ab010ad37a5e1eaf72dda8a85dd4e
suvam <suvampravu@gmail.com>
2024-10-02 22:03:56 +0530

    Day10 in python

---
+++ Day-10.py
@@ -0,0 +1,14 @@
+# video-20 functions
+def calculateMean(a, b):
+    return (a + b) / 2
+
+
+def isGreater(a, b):
+    if a > b:
+        print("a is greater")
+    else:
+        print("b is greater")
+
+
+print(calculateMean(4, 5))
+isGreater(4,5)
