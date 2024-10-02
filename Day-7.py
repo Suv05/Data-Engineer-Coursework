4e61d13e7ac473507d7fc7b888c62b0c2bdc62a4
suvam <suvampravu@gmail.com>
2024-10-02 22:04:15 +0530

    commit

---
+++ Day-7.py
@@ -0,0 +1,18 @@
+# loop video-17
+
+n = "name"
+for i in n:
+    print(i)
+
+a = ["a", "b", "c", "d"]
+for i in a:
+    print(i)
+
+for k in range(5):
+    print(k + 1)
+
+for k in range(1, 5):
+    print(k ** 2)
+    
+for k in range(1, 10, +2):
+    print(k + 1)
