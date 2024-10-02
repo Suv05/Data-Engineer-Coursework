f41dd3351a752ff34fc9707185dcdb0b429566ce
suvam <suvampravu@gmail.com>
2024-10-02 22:04:04 +0530

    commit

---
+++ Day-5.py
@@ -0,0 +1,21 @@
+n = int(input("Enter a number: "))
+
+# if
+if n == 18:
+    print("Ok")
+
+# if-else
+if n == 18:
+    print("ok ok")
+else:
+    print("not ok")
+
+# if-elif-else
+if n == 18:
+    print("me ok")
+elif n > 18:
+    print("me more ok")
+else:
+    print("me not ok")
+
+#video 14 finished
