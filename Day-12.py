e4f63b80183ce049f71da539f84008f46d1b53b8
suvam <suvampravu@gmail.com>
2024-10-05 01:07:25 +0530

    commit

---
+++ Day-12.py
@@ -0,0 +1,21 @@
+# List methods
+l = [1, 2, 3, 4, 5, 6, 7, 8, 9,5,5]
+
+#l.append(8)
+#l.sort(reverse=True)
+#l.reverse()
+#print(l.index(5)) #means to print index of 5
+#print(l.count(5)) #means to count 5
+
+# m=l.copy() #returns  the copied list l
+# print(m)
+
+#concatination of list
+m=["suv","ety","writ"]
+m.extend(l)
+print(m)
+
+n=["wer","tyu","qax","tyi"]
+print(m+n)
+
+
