b5e6e10fa0ab31512c0f9058a39f966e2a893860
suvam <suvampravu@gmail.com>
2024-10-15 01:24:51 +0530

    commit changes

--- Day-18.py
+++ Day-18.py
@@ -4,15 +4,24 @@ dic = {
     "eligible": True
 }
 
-print(dic)
-print(dic["name"])
-print(dic.get("name"))
-a = dic.values()
-print(a)
-b = dic.keys()
-print(b)
-c=dic.items()
-print(c)
+# print(dic)
+# print(dic["name"])
+# print(dic.get("name"))
+# a = dic.values()
+# print(a)
+# b = dic.keys()
+# print(b)
+# c=dic.items()
+# print(c)
+#
+# for key in b:
+#     print(key)
 
-for key in b:
-    print(key)
+
+#methods
+dic.update({'name':22})
+#dic.clear()
+#dic.pop('name')
+#dic.popitem()
+del dic["name"]
+print(dic)
