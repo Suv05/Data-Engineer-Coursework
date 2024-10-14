55fed16b08e2ca61c39dea35e4abedf782721bca
suvam <suvampravu@gmail.com>
2024-10-14 20:35:56 +0530

    commit changes

---
+++ Day-18.py
@@ -0,0 +1,18 @@
+dic = {
+    "name": "karan",
+    "age": 17,
+    "eligible": True
+}
+
+print(dic)
+print(dic["name"])
+print(dic.get("name"))
+a = dic.values()
+print(a)
+b = dic.keys()
+print(b)
+c=dic.items()
+print(c)
+
+for key in b:
+    print(key)
