2d0a25d625f4f066cfef2149399398a9dcf5b825
suvam <suvampravu@gmail.com>
2024-10-15 01:24:58 +0530

    commit changes

---
+++ Day-19.py
@@ -0,0 +1,13 @@
+# for loop with else
+# video-35
+for i in range(6):
+    print(i)
+else:
+    print("sorry")
+
+for i in range(5):
+    print(i)
+    if i == 3:
+        break
+else:
+    print("sorry")
