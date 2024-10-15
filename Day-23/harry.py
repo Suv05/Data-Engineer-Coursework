b57c3a843bd4203f5462bac14fa89140d3da174a
suvam <suvampravu@gmail.com>
2024-10-16 02:08:23 +0530

    commit changes

---
+++ Day-23/harry.py
@@ -0,0 +1,8 @@
+#video-45
+
+import main
+
+
+print(__name__)
+main.welcome()
+main.goodbye()
---
+++ Day-23/main.py
@@ -0,0 +1,13 @@
+def welcome():
+    print("welcome home ")
+
+
+def goodbye():
+    print("good bye")
+
+
+print(__name__)
+
+if __name__ == "__main__":
+    welcome()
+    goodbye()
