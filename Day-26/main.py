83e40efcab3add2de2d6f52ffcd2e1306206ede5
suvam <suvampravu@gmail.com>
2024-10-19 01:32:49 +0530

    commit changes

--- Day-26/main.py
+++ Day-26/main.py
@@ -1,4 +1,4 @@
-# Video-49
+# Video-49&50&51
 
 # Read file
 # f=open("my-file1.txt","r")
@@ -10,8 +10,31 @@
 # f.write("I want to marry her")
 # f.close()
 
-#with keyword
-with open('my-file1.txt', 'r') as f: print(f.read())
+# with keyword
+# with open('my-file1.txt', 'r') as f: print(f.read())
+
+# readiness()
+# f = open('my-file1.txt', 'r')
+# while True:
+#     line = f.readline()
+#     if not line:
+#         break
+#     print(line)
+
+# writelines()
+# f = open('my-file3.txt', 'w')
+# lines=["line1\n", "line2\n","line3\n"]
+# f.writelines(lines)
+# f.close()
 
 
 
+#seek file and tell file function
+with open('my-file1.txt', 'r') as f:
+    print(type(f))
+    # move to  the 10th byte in the file
+    f.seek(10)
+
+    # read the next 5 bytes
+    data = f.read(5)
+    print(data)
