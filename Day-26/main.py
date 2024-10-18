442b52f7a46c8b13c7110675784364f9f75e6ce5
suvam <suvampravu@gmail.com>
2024-10-18 22:59:15 +0530

    commit changes

---
+++ Day-26/main.py
@@ -0,0 +1,17 @@
+# Video-49
+
+# Read file
+# f=open("my-file1.txt","r")
+# print(f.read())
+# f.close()
+
+# Write file
+# f=open('my-file1.txt',"w")
+# f.write("I want to marry her")
+# f.close()
+
+#with keyword
+with open('my-file1.txt', 'r') as f: print(f.read())
+
+
+
