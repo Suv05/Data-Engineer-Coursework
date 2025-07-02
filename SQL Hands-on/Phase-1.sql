015ca85e2064136b670725b8fb64478aeb9dce19
suvam <suvampravu@gmail.com>
2025-07-02 22:49:02 +0530

    phase-1 complete

--- SQL Hands-on/Phase-1.sql
+++ SQL Hands-on/Phase-1.sql
@@ -26,3 +26,26 @@ SELECT
 FROM customers
 ORDER BY registration_date DESC
 LIMIT 5;
+
+
+-- 3. Unique Countries
+-- → List all unique countries from the customers table
+SELECT DISTINCT country,first_name,last_name
+FROM customers;
+
+
+--4. High Order Amounts
+SELECT *
+FROM orders;
+
+-- → Fetch all orders where total_amount > 400
+SELECT * 
+FROM orders
+WHERE total_amount > 400
+
+
+-- 5. Products by Category
+-- → List all orders in the “Electronics” product category
+SELECT * 
+FROM orders
+WHERE product_category='Electronics';
