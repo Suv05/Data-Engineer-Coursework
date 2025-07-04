05be1aa4807c88f146be193ae0534dbc2640d05e
suvam <suvampravu@gmail.com>
2025-07-04 12:42:51 +0530

    completed phase-3

--- SQL Hands-on/Phase-3.sql
+++ SQL Hands-on/Phase-3.sql
@@ -1,9 +1,18 @@
 -- Active: 1751469617468@@127.0.0.1@5432@New_Data
-SELECT * FROM customers;
+SELECT
+    *
+FROM
+    customers;
 
-SELECT * FROM orders;
+SELECT
+    *
+FROM
+    orders;
 
-SELECT * FROM order_items;
+SELECT
+    *
+FROM
+    order_items;
 
 --11. Customer Order Details
 -- → List each order with customer’s name, email, and city.
@@ -17,4 +26,51 @@ FROM
     JOIN orders ON order_items.order_id = orders.order_id
     JOIN customers ON customers.customer_id = orders.customer_id;
 
---
+--12. Order with Items
+--→ Show each order_id with corresponding product_name and quantity
+SELECT
+    order_id,
+    product_name,
+    quantity
+FROM
+    order_items;
+
+--13. Customer with No Orders
+-- → List customers who haven't placed any orders
+SELECT
+    customer_id,
+    concat (first_name, ' ', last_name) AS customer_name
+FROM
+    customers
+WHERE
+    customer_id NOT IN (
+        SELECT
+            customer_id
+        FROM
+            orders
+    );
+
+--14. Orders with No Items
+-- → List orders that don't have any items linked.
+SELECT
+    os.order_id
+FROM
+    orders os
+    LEFT JOIN order_items oi ON os.order_id = oi.order_id
+WHERE
+    oi.item_id IS NULL;
+
+--15. Top Spend by Customer
+-- → Show top 5 customers based on total spend (unit_price * quantity)
+SELECT
+    concat (first_name, ' ', last_name) AS customer_name,
+    SUM(quantity * unit_price) AS total_spend
+FROM
+    customers c
+    JOIN orders os ON c.customer_id = os.customer_id
+    JOIN order_items oi ON os.order_id = oi.order_id
+GROUP BY
+    c.customer_id
+ORDER BY
+    total_spend DESC
+LIMIT(5);
